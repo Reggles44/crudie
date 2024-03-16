cb5333e26d0e67694900169f0ff6686702fcbd33
Reggles <reginaldbeakes@gmail.com>
2024-03-16 16:27:33 -0400

    rust gitignore

--- rust/sql/src/main.rs
+++ rust/sql/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+
+async fn create(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    println!("HELLO WORLD");
+    HttpServer::new(|| App::new().route("/create", web::post().to(create)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
